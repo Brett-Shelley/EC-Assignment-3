a5c03ed27e09d55849e10b67020d74e392c09285
protofall <david74437@gmail.com>
2019-11-05 16:50:11 +1030

    Mutator is basically done and crossover is starting

---
+++ ScatteredCrossover.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.awt.Point;
+import java.lang.Integer;
+import java.util.Random;
+import java.util.Collections;
+import java.lang.Math;
+
+// public class ScatteredCrossover implements Jmetal_crossover_interface
+public class ScatteredCrossover
+{
+	public ArrayList<Integer> crossover(){
+		return NULL;
+	}
+}
