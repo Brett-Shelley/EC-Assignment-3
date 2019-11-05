547b91cd213d73a883acbd56e07637557ec5f208
Live session user <a1705111@student.adelaide.edu.au>
2019-11-05 22:37:52 +1030

    Adds runner for ex4

---
+++ jMetal-master/jmetal-exec/src/main/java/org/uma/jmetal/runner/multiobjective/Ex4IBEARunner.java
@@ -0,0 +1,89 @@
+package org.uma.jmetal.runner.multiobjective;
+
+import org.uma.jmetal.algorithm.multiobjective.ibea.IBEA;
+import org.uma.jmetal.algorithm.multiobjective.ibea.IBEABuilder;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.SelectionOperator;
+import org.uma.jmetal.operator.impl.crossover.Ex4MeanCrossover;
+import org.uma.jmetal.operator.impl.crossover.SBXCrossover;
+import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
+import org.uma.jmetal.operator.impl.mutation.Ex4OffsetMutation;
+import org.uma.jmetal.operator.impl.selection.BinaryTournamentSelection;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.AbstractAlgorithmRunner;
+import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.ProblemUtils;
+
+import java.util.List;
+
+/**
+ * Class for configuring and running the IBEA algorithm
+ *
+ * @author Jayden Boskell
+ */
+public class IBEARunner extends AbstractAlgorithmRunner {
+  /**
+   * @param args Command line arguments.
+   * @throws java.io.IOException
+   * @throws SecurityException
+   * @throws ClassNotFoundException
+   * Invoking command:
+  java org.uma.jmetal.runner.multiobjective.IBEARunner problemName [referenceFront]
+   */
+  public static void main(String[] args) throws Exception {
+    Problem<DoubleSolution> problem;
+    IBEA<DoubleSolution> algorithm;
+    CrossoverOperator<DoubleSolution> crossover;
+    MutationOperator<DoubleSolution> mutation;
+    SelectionOperator<List<DoubleSolution>, DoubleSolution> selection;
+
+    String problemName ;
+    String referenceParetoFront = "" ;
+    if (args.length == 1) {
+      problemName = args[0];
+    } else if (args.length == 2) {
+      problemName = args[0] ;
+      referenceParetoFront = args[1] ;
+    } else {
+      problemName = "org.uma.jmetal.problem.multiobjective.Ex4_Problem";
+      referenceParetoFront = "" ;
+    }
+
+    problem = ProblemUtils.loadProblem(problemName);
+
+    double crossoverProbability = 0.9 ;
+    double crossoverDistributionIndex = 20.0 ;
+    crossover = new Ex4MeanCrossover(crossoverProbability, crossoverDistributionIndex) ;
+
+    double mutationProbability = 1.0 / problem.getNumberOfVariables() ;
+    double mutationDistributionIndex = 20.0 ;
+    mutation = new Ex4OffsetMutation(mutationProbability, mutationDistributionIndex) ;
+
+    selection = new BinaryTournamentSelection<DoubleSolution>() ;
+
+    algorithm = new IBEABuilder(problem)
+      .setArchiveSize(100)
+      .setPopulationSize(100)
+      .setMaxEvaluations(25000)
+      .setCrossover(crossover)
+      .setMutation(mutation)
+      .setSelection(selection)
+      .build() ;
+
+    AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
+      .execute() ;
+
+    List<DoubleSolution> population = algorithm.getResult() ;
+    long computingTime = algorithmRunner.getComputingTime() ;
+
+    JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
+
+    printFinalSolutionSet(population);
+    if (!referenceParetoFront.equals("")) {
+      printQualityIndicators(population, referenceParetoFront) ;
+    }
+  }
+}
